fdaabb865e369cf75530526e22f62630368a3bd7
hechfx <hechfx@hotmail.com>
2022-03-20 21:59:50 -0300

    Refactor some files and adding RobloxCommand.kt

---
+++ src/main/kotlin/me/hechfx/konnor/database/DatabaseService.kt
@@ -0,0 +1,28 @@
+package me.hechfx.konnor.database
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import me.hechfx.konnor.config.database.DatabaseConfig
+import me.hechfx.konnor.database.table.Users
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.sql.update
+
+object PostgreSQL {
+    fun startDatabase(databaseConfig: DatabaseConfig) {
+        val config = HikariConfig().apply {
+            jdbcUrl = "jdbc:postgresql://${databaseConfig.host}:${databaseConfig.port}/${databaseConfig.database}?useTimezone=true&serverTimezone=UTC"
+            driverClassName = "org.postgresql.Driver"
+            username = databaseConfig.user
+            password = databaseConfig.password
+            maximumPoolSize = 8
+        }
+
+        Database.connect(HikariDataSource(config))
+
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(Users)
+        }
+    }
+}
